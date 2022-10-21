6db2c95fa46f492aa70130837cc23185512466e0
Goldfish808 <tjddns0853@gmail.com>
2022-10-21 17:09:10 +0900

    [Hibernate] CRUD 테스트 완료

--- src/main/java/site/metacoding/white/domain/BoardRepository.java
+++ src/main/java/site/metacoding/white/domain/BoardRepository.java
@@ -1,8 +1,11 @@
 package site.metacoding.white.domain;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,8 +15,27 @@ public class BoardRepository {
 
     private final EntityManager sM;
 
+    @Transactional
     public void save(Board board) {
-        sM.persist(board); // Insert 쿼리문이 동작하면서 Insert를 해줌
+        sM.persist(board);
+    }
 
+    public void deleteById(Long id) {
+        sM.createQuery("DELETE from Board b where b.id =:id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+
+    public Board findById(Long id) {
+        Board boardPS = sM.createQuery("select b from Board b where b.id =:id", Board.class)
+                .setParameter("id", id)
+                .getSingleResult();
+        return boardPS;
+    }
+
+    public List<Board> findAll() {
+        List<Board> boardPS = sM.createQuery("select b from Board b", Board.class)
+                .getResultList();
+        return boardPS;
     }
 }
---
+++ src/main/java/site/metacoding/white/web/BoardApiController.java
@@ -0,0 +1,49 @@
+package site.metacoding.white.web;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.white.domain.Board;
+import site.metacoding.white.domain.BoardRepository;
+import site.metacoding.white.service.BoardService;
+
+@RequiredArgsConstructor
+@RestController
+public class BoardApiController {
+
+    private final BoardService boardService;
+
+    @GetMapping(value = { "/board/{id}", "/board" })
+    public Object findById(@PathVariable(required = false) Long id) {
+        if (id != null) {
+            return boardService.findById(id);
+        }
+        return boardService.findAll();
+
+    }
+
+    @PutMapping("/board/{id}")
+    public String update(@PathVariable Long id, @RequestBody Board board) {
+        boardService.update(id, board);
+        return "ok";
+    }
+
+    @DeleteMapping("/board/{id}")
+    public String delete(@PathVariable Long id) {
+        boardService.deleteById(id);
+        return "ok";
+    }
+
+    @PostMapping("/board")
+    public String save(@RequestBody Board board) {
+        boardService.save(board);
+        return "Ok";
+    }
+}
